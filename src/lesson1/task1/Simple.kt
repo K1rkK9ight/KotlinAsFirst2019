55392a88260272c1e7d39681f5938914db269161
K1rkK9ght <andrianov.vital@yandex.ru>
2019-09-13 22:32:56 +0300

    =_=

--- src/lesson1/task1/Simple.kt
+++ src/lesson1/task1/Simple.kt
@@ -78,7 +78,7 @@ fun lengthInMeters(sagenes: Int, arshins: Int, vershoks: Int): Double = 213.36 *
  * Пользователь задает угол в градусах, минутах и секундах (например, 36 градусов 14 минут 35 секунд).
  * Вывести значение того же угла в радианах (например, 0.63256).
  */
-fun angleInRadian(deg: Int, min: Int, sec: Int): Double = ((deg + 60 * min + 3600 * sec)* PI) / 180
+fun angleInRadian(deg: Int, min: Int, sec: Int): Double = ((deg + min / 60 + sec / 3600) * PI) / 180
 
 /**
  * Тривиальная
@@ -86,7 +86,7 @@ fun angleInRadian(deg: Int, min: Int, sec: Int): Double = ((deg + 60 * min + 360
  * Найти длину отрезка, соединяющего точки на плоскости с координатами (x1, y1) и (x2, y2).
  * Например, расстояние между (3, 0) и (0, 4) равно 5
  */
-fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1-x2) + sqr(y1-y2))
+fun trackLength(x1: Double, y1: Double, x2: Double, y2: Double): Double = sqrt(sqr(x1 - x2) + sqr(y1 - y2))
 
 /**
  * Простая
@@ -113,7 +113,8 @@ fun travelMinutes(hoursDepart: Int, minutesDepart: Int, hoursArrive: Int, minute
  * Сколько денег будет на счету через 3 года (с учётом сложных процентов)?
  * Например, 100 рублей под 10% годовых превратятся в 133.1 рубля
  */
-fun accountInThreeYears(initial: Int, percent: Int): Double = (initial*((percent%100)+1) * ((percent%100)+1) * ((percent%100)+1))
+fun accountInThreeYears(initial: Int, percent: Int): Double = initial * (1 + percent / 100.0) * (1 + percent / 100.0) *
+        (1 + percent / 100.0)
 
 /**
  * Простая
@@ -121,4 +122,4 @@ fun accountInThreeYears(initial: Int, percent: Int): Double = (initial*((percent
  * Пользователь задает целое трехзначное число (например, 478).
  * Необходимо вывести число, полученное из заданного перестановкой цифр в обратном порядке (например, 874).
  */
-fun numberRevert(number: Int): Int =
+fun numberRevert(number: Int): Double = (number % 10.0) * 100 + (number / 10.0) * 10 + (number / 100.0)
