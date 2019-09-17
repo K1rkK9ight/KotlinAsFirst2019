fd1d3849ddee9bab22b2274f0d44a86cbaae0d6c
K1rkK9ght <andrianov.vital@yandex.ru>
2019-09-17 23:37:37 +0300

    3

--- src/lesson2/task2/Logical.kt
+++ src/lesson2/task2/Logical.kt
@@ -18,7 +18,11 @@ fun pointInsideCircle(x: Double, y: Double, x0: Double, y0: Double, r: Double) =
  * Четырехзначное число назовем счастливым, если сумма первых двух ее цифр равна сумме двух последних.
  * Определить, счастливое ли заданное число, вернуть true, если это так.
  */
-fun isNumberHappy(number: Int): Boolean = TODO()
+fun isNumberHappy(number: Int): Boolean {
+    if ((number / 1000) + ((number % 1000) / 100) == (number % 100) +
+        ((number % 10) / 10)) return true
+
+
 
 /**
  * Простая
@@ -36,7 +40,9 @@ fun queenThreatens(x1: Int, y1: Int, x2: Int, y2: Int): Boolean = TODO()
  * Дан номер месяца (от 1 до 12 включительно) и год (положительный).
  * Вернуть число дней в этом месяце этого года по григорианскому календарю.
  */
-fun daysInMonth(month: Int, year: Int): Int = TODO()
+fun daysInMonth(month: Int, year: Int): Int =
+
+}
 
 /**
  * Средняя
